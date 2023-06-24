6a5cb9870f8b5e0dba4ad90d79df97af073db8b2
clement <c.gambier@gustosolutions.fr>
2023-06-24 16:23:41 +0200

    https

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -33,8 +33,8 @@ export default {
   methods: {
     fetchProducts() {
       axios
-        // .get("http://localhost:8888/router.php?route=products")
-        .get("http://s3-4959.nuage-peda.fr/eval/router.php?route=products")
+        .get("https://s3-4959.nuage-peda.fr/eval/router.php?route=products")
+        // .get("http://s3-4959.nuage-peda.fr/eval/router.php?route=products")
         .then((response) => {
           this.products = response.data;
         })
